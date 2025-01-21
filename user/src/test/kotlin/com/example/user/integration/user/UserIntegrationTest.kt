b65ed0eebf728ff4c52a0b5a5af021bf28adc7b1
rvlwldev <hyungjun@gimhyeongjun-ui-noteubug.local>
2025-01-22 00:32:28 +0900

    refactor(user): UserIntegrationTest.kt refactor codes

--- user/src/test/kotlin/com/example/user/integration/user/UserIntegrationTest.kt
+++ user/src/test/kotlin/com/example/user/integration/user/UserIntegrationTest.kt
@@ -1,18 +1,20 @@
 package com.example.user.integration.user
 
+import com.example.user.domain.history.HistoryRepository
+import com.example.user.domain.user.UserRepository
+import com.example.user.infrastructure.jpa.UserHistoryJpaRepository
 import com.example.user.infrastructure.jpa.UserJpaRepository
 import org.junit.jupiter.api.BeforeEach
-import org.junit.jupiter.api.assertThrows
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.web.client.TestRestTemplate
 import org.springframework.boot.test.web.server.LocalServerPort
 import org.springframework.http.HttpStatus
 import org.springframework.test.context.ActiveProfiles
-import org.springframework.web.client.HttpClientErrorException
-import org.springframework.web.client.RestTemplate
 import kotlin.test.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertNotNull
+import kotlin.test.assertTrue
 
 @ActiveProfiles("test")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -21,31 +23,49 @@ class UserIntegrationTest {
     @LocalServerPort
     private var port: Int = 0
 
-    @Autowired
-    private lateinit var jpa: UserJpaRepository
+    private val restTemplate = TestRestTemplate()
 
-    private val restTemplate = RestTemplate()
+    @Autowired
+    private lateinit var repo: UserRepository
+
+    @Autowired
+    private lateinit var historyRepository: HistoryRepository
+
+    @Autowired
+    private lateinit var historyJpa: UserHistoryJpaRepository
+
+    @Autowired
+    private lateinit var userJpa: UserJpaRepository
 
     @BeforeEach
     fun setUp() {
-        jpa.deleteAll()
+        userJpa.deleteAll()
+        historyJpa.deleteAll()
     }
 
     @Test
     fun `request save user - success`() {
+        val userId = "testId"
+        val userName = "testName"
         val url = "http://localhost:$port/users"
-        val req = mapOf("id" to "testId", "name" to "testName")
+        val req = mapOf("id" to userId, "name" to userName)
         val res = restTemplate.postForEntity(url, req, Map::class.java)
 
         assertEquals(HttpStatus.CREATED, res.statusCode)
         assertNotNull(res.body)
-        assertEquals("testId", res.body!!["id"])
-        assertEquals("testName", res.body!!["name"])
+        assertEquals(userId, res.body!!["id"])
+        assertEquals(userName, res.body!!["name"])
 
-        val user = jpa.findById("testId").orElse(null)
+        val user = repo.find(userId)
         assertNotNull(user)
-        assertEquals("testId", user.id)
-        assertEquals("testName", user.name)
+        assertEquals(userId, user.id)
+        assertEquals(userName, user.name)
+
+        val histories = historyJpa.findAll()
+        assertTrue(histories.isNotEmpty())
+
+        val history = histories.first()
+        assertEquals(userName, history.newName)
     }
 
     @Test
@@ -53,21 +73,15 @@ class UserIntegrationTest {
         val url = "http://localhost:$port/users"
 
         val req1 = mapOf("id" to "", "name" to "testName")
-        val res1 = assertThrows<HttpClientErrorException> {
-            restTemplate.postForEntity(url, req1, Map::class.java)
-        }
+        val res1 = restTemplate.postForEntity(url, req1, Map::class.java)
         assertEquals(HttpStatus.BAD_REQUEST, res1.statusCode)
 
         val req2 = mapOf("id" to "testId", "name" to "")
-        val res2 = assertThrows<HttpClientErrorException> {
-            restTemplate.postForEntity(url, req2, Map::class.java)
-        }
+        val res2 = restTemplate.postForEntity(url, req2, Map::class.java)
         assertEquals(HttpStatus.BAD_REQUEST, res2.statusCode)
 
         val req3 = mapOf("id" to "testId", "name" to "tes")
-        val res3 = assertThrows<HttpClientErrorException> {
-            restTemplate.postForEntity(url, req3, Map::class.java)
-        }
+        val res3 = restTemplate.postForEntity(url, req3, Map::class.java)
         assertEquals(HttpStatus.BAD_REQUEST, res3.statusCode)
     }
 
