1b9d84d43f47024feda93d29987f8f4eb062e2d9
rvlwldev <hyungjun@gimhyeongjun-ui-noteubug.local>
2025-03-10 21:32:39 +0900

    feat(user): set Transaction when return user JWT after user signup

--- user/src/main/kotlin/com/example/user/application/AuthFacade.kt
+++ user/src/main/kotlin/com/example/user/application/AuthFacade.kt
@@ -3,10 +3,12 @@ package com.example.user.application
 import com.example.user.domain.UserService
 import com.example.user.interfaces.auth.AuthResponse
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 @Service
 class AuthFacade(private val service: UserService) {
 
+    @Transactional
     fun signUp(username: String, password: String) =
         service.signUp(username, password)
             .flatMap { user -> service.signIn(username, password) }
