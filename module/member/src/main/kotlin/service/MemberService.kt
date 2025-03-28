c670eb7e6c8bd7c4e105443e0bd038784061b91b
rvlwldev <hyungjun@gimhyeongjun-ui-noteubug.local>
2025-03-28 23:10:30 +0900

    feat: implement Member service methods

--- module/member/src/main/kotlin/service/MemberService.kt
+++ module/member/src/main/kotlin/service/MemberService.kt
@@ -1,7 +1,37 @@
 package service
 
 import org.springframework.stereotype.Service
+import java.util.UUID
 
 @Service
-class MemberService {
+class MemberService(private val repo: MemberRepository) {
+
+    fun create(name: String, email: String, password: String) =
+        repo.save(Member(name, email, password))
+
+    fun find(uuid: UUID) =
+        repo.find(uuid) ?: throw IllegalArgumentException()
+
+    fun chargeCash(uuid: UUID, amount: Long): Member {
+        val member = repo.find(uuid) ?: throw IllegalArgumentException()
+
+        member.increaseCash(amount)
+
+        return member
+    }
+
+    fun useCash(uuid: UUID, amount: Long): Member {
+        val member = repo.find(uuid) ?: throw IllegalArgumentException()
+
+        member.decreaseCash(amount)
+
+        return member
+    }
+
+    fun withdraw(uuid: UUID, password: String) {
+        val member = repo.find(uuid) ?: throw IllegalArgumentException()
+
+        repo.delete(member)
+    }
+
 }
