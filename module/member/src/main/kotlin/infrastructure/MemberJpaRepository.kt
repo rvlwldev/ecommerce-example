de99dbe84832ccfa424f12a91027217002b13316
rvlwldev <hyungjun@gimhyeongjun-ui-noteubug.local>
2025-03-28 23:15:26 +0900

    feat: implement Member Repository with JPA

--- module/member/src/main/kotlin/infrastructure/MemberJpaRepository.kt
+++ module/member/src/main/kotlin/infrastructure/MemberJpaRepository.kt
@@ -2,6 +2,9 @@ package infrastructure
 
 import org.springframework.data.jpa.repository.JpaRepository
 import service.Member
+import java.util.Optional
 import java.util.UUID
 
-interface MemberJpaRepository : JpaRepository<UUID, Member>
+interface MemberJpaRepository : JpaRepository<Member, UUID> {
+    fun findByEmailAndPassword(email: String, password: String): Optional<Member>
+}
