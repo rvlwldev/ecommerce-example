f9a542893486bf973f96ee4dc50315e303b28880
rvlwldev <hyungjun@gimhyeongjun-ui-noteubug.local>
2025-03-29 01:40:46 +0900

    chore: fix wrong dependency name

--- module/member/build.gradle.kts
+++ module/member/build.gradle.kts
@@ -2,5 +2,5 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    runtimeOnly("mysql:mysql-connector-j")
+    runtimeOnly("com.mysql:mysql-connector-j")
 }
