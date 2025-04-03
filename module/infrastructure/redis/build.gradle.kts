c42ccbc75a24c58dd415a56bd4ac29feec1ad673
rvlwldev <hyungjun@gimhyeongjun-ui-noteubug.local>
2025-04-04 00:28:23 +0900

    feat: implement DistributedLock in redis module

--- module/infrastructure/redis/build.gradle.kts
+++ module/infrastructure/redis/build.gradle.kts
@@ -1,8 +1,13 @@
+tasks.withType<JavaCompile>().configureEach {
+    options.compilerArgs.add("-parameters")
+}
+
 plugins {
     alias(libs.plugins.spring.boot)
     alias(libs.plugins.spring.dependency.management)
 }
 
 dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-aop")
     implementation("org.springframework.boot:spring-boot-starter-data-redis")
 }
---
+++ module/infrastructure/redis/src/main/kotlin/DistributedLockClient.kt
@@ -0,0 +1,29 @@
+import configuration.DistributedLockProperty
+import org.springframework.data.redis.core.RedisTemplate
+import org.springframework.stereotype.Component
+import java.time.Duration
+
+@Component
+class DistributedLockClient(
+    private val redis: RedisTemplate<String, Boolean>,
+    private val property: DistributedLockProperty
+) {
+
+    fun acquireLock(key: String): Boolean {
+        val lockKey = "${property.prefix}:$key"
+        val timeout = Duration.ofMillis(property.timeout)
+
+        repeat(property.retry) {
+            val success = redis.opsForValue().setIfAbsent(lockKey, true, timeout)
+            if (success == true) return true
+
+            Thread.sleep(property.delay)
+        }
+
+        return false
+    }
+
+    fun releaseLock(key: String) =
+        redis.delete(key)
+
+}
