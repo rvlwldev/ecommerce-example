4f288b7163fbf136ae460506abce1ee7838b53c5
rvlwldev <hyungjun@gimhyeongjun-ui-noteubug.local>
2025-04-20 21:56:22 +0900

    feat: implement common exceptions

--- module/core/exception/src/main/kotlin/com/ecommerce/CommonException.kt
+++ module/core/exception/src/main/kotlin/com/ecommerce/CommonException.kt
@@ -1,17 +1,20 @@
 package com.ecommerce
 
-class CommonException(
+open class CommonException(
     val code: String,
     override val message: String
 ) : RuntimeException(message) {
 
-    constructor() : this(
-        code = "500",
-        message = "Unexpected Internal Server Error Occurred"
-    )
+    constructor(code: Any, message: String) : this(code.toString(), message)
+    constructor(message: String) : this("UNKNOWN", message)
+    constructor() : this(500, "Unexpected Internal Server Error Occurred")
 
-    constructor(error: BaseError) : this(error.code, error.message)
-
-    constructor(message: String) : this(code = "UNKNOWN", message = message)
+    class InvalidValue(message: String = "Invalid Input Format") : CommonException(400, message)
+    class RequiredValue(message: String = "Required Value is Empty") : CommonException(400, message)
+    class Unauthorized(message: String = "Unauthorized access") : CommonException(401, message)
+    class Forbidden(message: String = "Forbidden Data") : CommonException(401, message)
+    class NotFound(message: String = "Data Not Found") : CommonException(404, message)
+    class AlreadyExists(message: String = "Duplicated Data") : CommonException(409, message)
+    class InvalidState(message: String = "Invalid Data State, Already Done Or Cancelled") : CommonException(409, message)
 
 }
