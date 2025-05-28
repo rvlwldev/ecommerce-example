e79e3e6ffdf5c7748075f3ae15dd7f19078e050b
rvlwldev <hyungjun@gimhyeongjun-ui-noteubug.local>
2025-05-29 03:25:22 +0900

    feat: add validation of timeout, delay, retry

--- module/common/redis/src/main/kotlin/com/ecommerce/DistributedLockClient.kt
+++ module/common/redis/src/main/kotlin/com/ecommerce/DistributedLockClient.kt
@@ -11,14 +11,22 @@ class DistributedLockClient<T>(
     private val property: DistributedLockProperty
 ) {
 
+    private fun validateLockSettings(timeout: Long, delay: Long, retry: Int) {
+        require(timeout > 0) { String.format(INVALID_TIMEOUT_VALUE_MESSAGE, timeout) }
+        require(delay >= 0) { String.format(INVALID_DELAY_VALUE_MESSAGE, delay) }
+        require(retry >= 0) { String.format(INVALID_RETRY_VALUE_MESSAGE, retry) }
+    }
+
     fun acquire(
+        prefix: String,
         key: T,
-        prefix: String = property.prefix,
         timeout: Long = property.timeout,
         delay: Long = property.delay,
         retry: Int = property.retry
     ): Boolean {
-        val lockKey = "$prefix:${key.toString()}"
+        validateLockSettings(timeout, delay, retry)
+
+        val lockKey = "${property.prefix}:$prefix:${key.toString()}"
         val timeout = Duration.ofMillis(timeout)
 
         repeat(retry) {
@@ -34,9 +42,16 @@ class DistributedLockClient<T>(
     fun release(key: T, prefix: String = property.prefix) =
         redis.delete("$prefix:${key.toString()}")
 
-    fun <R> runWithLock(key: T, action: () -> R) =
+    fun <R> runWithLock(
+        prefix: String,
+        key: T,
+        action: () -> R,
+        timeout: Long = property.timeout,
+        delay: Long = property.delay,
+        retry: Int = property.retry
+    ) =
         try {
-            acquire(key)
+            acquire(prefix, key, timeout, delay, retry)
             action()
         } finally {
             release(key)
@@ -47,6 +62,9 @@ class DistributedLockClient<T>(
 
     companion object {
         const val LOCK_ALREADY_HELD_MESSAGE = "Failed to acquire lock. Lock already held for key: %s"
+        const val INVALID_TIMEOUT_VALUE_MESSAGE = "Timeout must be positive : %d"
+        const val INVALID_DELAY_VALUE_MESSAGE = "Timeout must be non-negative : %d"
+        const val INVALID_RETRY_VALUE_MESSAGE = "Timeout must be non-negative : %d"
     }
 
 }
