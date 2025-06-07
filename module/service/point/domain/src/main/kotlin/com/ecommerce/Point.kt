f7c3d3388d0b9521252e9eb572e77ba2b3862d59
rvlwldev <hyungjun@gimhyeongjun-ui-noteubug.local>
2025-06-07 21:16:25 +0900

    feat: implement separated point from user domain

--- module/service/point/domain/src/main/kotlin/com/ecommerce/Point.kt
+++ module/service/point/domain/src/main/kotlin/com/ecommerce/Point.kt
@@ -3,25 +3,34 @@ package com.ecommerce
 import java.util.UUID
 
 class Point(
-    val userId: UUID = UUID.fromString(""),
+    val userId: UUID,
     amount: Long = 0
 ) {
     var amount = amount
         private set
 
-    fun validateAmount(amount: Long) =
-        require(amount > 0L && amount % 10L == 0L) { PointError.INVALID_CASH_AMOUNT }
+    val chargeUnit = 10L
 
-    fun increaseCash(amount: Long) {
+    fun validateAmount(amount: Long) {
+        require(amount > 0L) { PointError.INVALID_POINT_AMOUNT }
+    }
+
+    fun charge(amount: Long) {
         validateAmount(amount)
+
+        require(amount % chargeUnit == 0L) {
+            String.format(PointError.INVALID_CHARGE_AMOUNT_UNIT, chargeUnit)
+        }
 
         this.amount += amount
     }
 
-    fun decreaseCash(amount: Long) {
+    fun use(amount: Long) {
         validateAmount(amount)
-        require(amount >= this.amount) { PointError.NOT_ENOUGH_CASH }
+
+        require(this.amount >= amount) { PointError.NOT_ENOUGH_POINT }
 
         this.amount -= amount
     }
+
 }
