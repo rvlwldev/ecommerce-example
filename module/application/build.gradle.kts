e9244de72590f0cdf4ec87103ca87919aef25b22
rvlwldev <hyungjun@gimhyeongjun-ui-noteubug.local>
2025-06-07 18:10:58 +0900

    chore: organize main app and dependencies

---
+++ module/application/build.gradle.kts
@@ -0,0 +1,6 @@
+dependencies {
+    implementation(project(":common-mysql"))
+    implementation(project(":common-redis"))
+    implementation(project(":common-kafka"))
+    implementation(project(":user-rest"))
+}
