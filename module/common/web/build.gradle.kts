abd7a0c193122462a27c33c1fcc7adf57edb5cfc
rvlwldev <hyungjun@gimhyeongjun-ui-noteubug.local>
2025-06-07 18:09:05 +0900

    chore: delete useless address module

--- module/common/web/build.gradle.kts
+++
@@ -1,9 +0,0 @@
-plugins {
-    alias(libs.plugins.spring.boot)
-    alias(libs.plugins.spring.dependency.management)
-}
-
-dependencies {
-    api("org.springframework.boot:spring-boot-starter")
-    api("org.springframework.boot:spring-boot-starter-web")
-}
