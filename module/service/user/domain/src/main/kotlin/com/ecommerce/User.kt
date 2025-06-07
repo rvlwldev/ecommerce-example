88320268a7cfa60a972bdd7d7b1bd20d9c52bce2
rvlwldev <hyungjun@gimhyeongjun-ui-noteubug.local>
2025-06-07 21:15:08 +0900

    feat: implement user domain

--- module/service/user/domain/src/main/kotlin/com/ecommerce/User.kt
+++ module/service/user/domain/src/main/kotlin/com/ecommerce/User.kt
@@ -1,12 +1,13 @@
 package com.ecommerce
 
 import java.util.UUID
+import kotlin.text.Regex
 
 class User(
     val id: UUID = UUID.randomUUID(),
     name: String = "",
     email: String = "",
-    encryptedPassword: String = "",
+    password: String = "",
 ) {
     var name = name
         private set
@@ -14,32 +15,50 @@ class User(
     var email: String = email
         private set
 
-    var encryptedPassword: String = encryptedPassword
+    var password: String = password
         private set
 
-    constructor(name: String, email: String, encryptedPassword: String) : this() {
+    constructor(name: String, email: String, password: String) : this() {
+        validateName(name)
         validateEmail(email)
+        validatePassword(password)
 
         this.name = name
         this.email = email
-        this.encryptedPassword = encryptedPassword
+        this.password = password
     }
 
-    fun validateEmail(email: String) =
-        require(Regex("^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$").matches(email)) { UserError.INVALID_EMAIL }
+    fun validateName(name: String) {
+        require(name.isNotEmpty()) { UserError.NOT_EMPTY_NAME }
+        require(name.length >= 2) { UserError.INVALID_NAME_LENGTH }
+        require(name != this.name) { UserError.DUPLICATE_NAME }
+    }
+
+    fun updateName(name: String) {
+        validateName(name)
+        this.name = name
+    }
+
+    fun validateEmail(email: String) {
+        val regex = Regex("^[A-Za-z0-9+_.-]+@([A-Za-z0-9.-]+|\\[[0-9]{1,3}(\\.[0-9]{1,3}){3}])$")
+
+        require(email.isNotEmpty()) { UserError.NOT_EMPTY_EMAIL }
+        require(regex.matches(email)) { UserError.INVALID_EMAIL }
+        require(email != this.email) { UserError.DUPLICATE_EMAIL }
+    }
 
     fun updateEmail(newEmail: String) {
         validateEmail(newEmail)
-
         email = newEmail
     }
 
-    fun validateName(name: String) = require(name.isNotEmpty()) { UserError.NOT_EMPTY_NAME }
+    fun validatePassword(password: String) {
+        require(password.isNotEmpty()) { UserError.NOT_EMPTY_PASSWORD }
+    }
 
-    fun updateName(newName: String) {
-        validateName(newName)
-
-        name = newName
+    fun updatePassword(password: String) {
+        validatePassword(password)
+        this.password = password
     }
 
 }
